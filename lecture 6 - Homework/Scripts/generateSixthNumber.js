0927da0846b3f3db0a78b216c140234754d8b127
Lyuboslav Petrov <104424313+LyuboPetrov94@users.noreply.github.com>
2022-07-27 09:12:35 +0300

    Update generateSixthNumber.js

--- lecture 6 - Homework/Scripts/generateSixthNumber.js
+++ lecture 6 - Homework/Scripts/generateSixthNumber.js
@@ -1,11 +1,11 @@
 function generateSixthNumber(n1, n2, n3, n4, n5,)
 {
-    let sum = (n1 + n2 + n3 +n4 + n5);
+    let sum = +(n1 + n2 + n3 +n4 + n5);
     
     if(isNumberTwoDigit(sum))
     {
-        let firstDigit = Math.floor(sum / 10);
-        let secondDigit = sum % 10;
+        let firstDigit = +(Math.floor(sum / 10));
+        let secondDigit = +(sum % 10);
         return firstDigit + secondDigit;
     }
 
